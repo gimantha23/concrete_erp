7a108c7aab9e5ed4ea708b6115ff72fddbc4b887
gima <gimantha.k98@gmail.com>
2021-08-13 20:52:39 +0530

    added reload page

--- Pages/view_order_details.php
+++ Pages/view_order_details.php
@@ -49,6 +49,7 @@ $job_no = ($_GET['jobid']);
                 req.open("POST", "../PHPScripts/approve_order.php", true);
                 req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 req.send("jobno=" + jobno);
+                location.reload();
             }
         }
     }
